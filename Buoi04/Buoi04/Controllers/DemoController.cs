948ea5762e44a7ccc6b2885587ab14b8c030b226
Hien Luong Tran Hy <hyhien@gmail.com>
2025-06-20 12:00:58 +0700

    Buoi 04 - Lab03 BT2

---
+++ Buoi04/Buoi04/Controllers/DemoController.cs
@@ -0,0 +1,33 @@
+﻿using Buoi04.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Buoi04.Controllers
+{
+	public class DemoController : Controller
+	{
+		public async Task<IActionResult> AsyncDemo()
+		{
+			var sw = new Stopwatch();
+			sw.Start();
+			var a = MyDemo.FuncAAsync();
+			var b = MyDemo.FuncBAsync();
+			var c = MyDemo.FuncCAsync();
+			await a; await b; await c;
+			sw.Stop();
+
+			return Content($"Chạy hết {sw.ElapsedMilliseconds}ms");
+		}
+
+		public IActionResult SyncDemo()
+		{
+			var sw = new Stopwatch();
+			sw.Start();
+			MyDemo.FuncA();
+			MyDemo.FuncB();
+			MyDemo.FuncC();
+			sw.Stop();
+			return Content($"Chạy hết {sw.ElapsedMilliseconds} ms");
+		}
+	}
+}
---
+++ Buoi04/Buoi04/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using Buoi04.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Buoi04.Controllers
+{
+	public class StudentController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Manage(Student model, string LoaiFile)
+		{
+			//xử
+
+			return View(model);
+		}
+	}
+}
