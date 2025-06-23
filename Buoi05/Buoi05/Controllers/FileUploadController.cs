372a2e278233d2501d1e175442041e11d3cb3cc8
Hien Luong Tran Hy <hyhien@gmail.com>
2025-06-23 09:00:30 +0700

    Demo upload file

---
+++ Buoi05/Buoi05/Controllers/FileUploadController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Buoi05.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult UploadFile(IFormFile MyFile, string Description)
+        {
+            if (MyFile == null)
+            {
+                TempData["Message"] = "Có lỗi upload file.";
+            }
+            else
+            {
+                var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", MyFile.FileName);
+                using (var file = new FileStream(fullPath, FileMode.CreateNew))
+                {
+                    MyFile.CopyTo(file);
+                }
+                TempData["Message"] = "Upload file thành công.";
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult UploadFiles(List<IFormFile> MyFiles)
+        {
+            if (MyFiles == null)
+            {
+                TempData["Message"] = "Chưa có file để upload.";
+            }
+            else
+            {
+                foreach (var MyFile in MyFiles)
+                {
+                    var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", MyFile.FileName);
+                    using (var file = new FileStream(fullPath, FileMode.CreateNew))
+                    {
+                        MyFile.CopyTo(file);
+                    }
+                }
+                TempData["Message"] = "Upload file thành công.";
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Buoi05/README.md
@@ -0,0 +1,3 @@
+﻿# Buổi 05 (23/06/2025)
+
+## Demo Upload File - Lab 03 bài 3.4
