d2abefb7dec03f7300e80fa1c88184d8e5ad2a14
Hien Luong Tran Hy <hyhien@gmail.com>
2025-07-05 20:33:31 +0700

    EF Core Code First

---
+++ Buoi09/Lab06/Models/Loai.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab06.Models
+{
+	[Table("Loai")]
+	public class Loai
+	{
+		[Key]
+		public int MaLoai { get; set; }
+
+		[MaxLength(100)]
+		public string TenLoai { get; set; }
+		public string? MoTa { get; set; }
+		public string? Hinh { get; set; }
+	}
+}
---
+++ Buoi09/Lab06/Models/MyDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lab06.Models
+{
+	public class MyDbContext : DbContext
+	{
+		public DbSet<Loai> Loais { get; set; }
+		public DbSet<HangHoa> HangHoas { get; set;}
+
+		public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+	}
+}
