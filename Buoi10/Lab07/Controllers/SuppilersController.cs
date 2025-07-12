5688f4e3e7f4eb8aa115341dfaba4bbcca4ef88c
Hien Luong Tran Hy <hyhien@gmail.com>
2025-07-12 16:10:18 +0700

    Add Supplier

--- Buoi10/Lab07/Controllers/SuppilersController.cs
+++ Buoi10/Lab07/Controllers/SuppilersController.cs
@@ -1,4 +1,5 @@
 ﻿using Lab07.Data;
+using Lab07.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Lab07.Controllers
@@ -12,10 +13,40 @@ namespace Lab07.Controllers
 			_context = context;
 		}
 
-		public IActionResult Index()
+		public IActionResult Index(string? TuKhoa)
 		{
-			var data = _context.Suppliers.ToList();
-			return View(data);
+			var data = _context.Suppliers.AsQueryable();
+			if (TuKhoa != null)
+			{
+				data = data.Where(s => s.Name.Contains(TuKhoa) || s.Email.Contains(TuKhoa) || s.Phone.Contains(TuKhoa));
+			}
+			return View(data.ToList());
+		}
+
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Create(Supplier model, IFormFile Logo)
+		{
+			try
+			{
+				if (Logo != null)
+				{
+					model.Logo = MyTool.UploadImageToFolder(Logo, "Suppliers");
+				}
+				_context.Add(model);
+				_context.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			catch (Exception ex)
+			{
+				ViewBag.Exception = "Lỗi: " + ex.Message;
+				return View();
+			}
 		}
 	}
 }
