c42625d3ccaead6a452ff85a2a27cc897fe0d84c
Hien Luong Tran Hy <hyhien@gmail.com>
2025-08-01 11:45:55 +0700

    Demo Ajax

---
+++ MyEShop01/MyEShop01/Controllers/AjaxController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyEShop01.Entities;
+using MyEShop01.Models;
+
+namespace MyEShop01.Controllers
+{
+	public class AjaxController : Controller
+	{
+		private readonly MyEshopContext _context;
+
+		public AjaxController(MyEshopContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Search()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Search(string keyword)
+		{
+			var dsHangHoa = _context.HangHoas.Where(p => p.TenHh.Contains(keyword));
+
+			var data = dsHangHoa.Select(hh => new KetQuaTimKiemVM
+			{
+				MaHh = hh.Id,
+				TenHh = hh.TenHh,
+				Hinh = hh.Hinh,
+				DonGia = hh.DonGia.Value,
+				NgaySX = hh.NgaySx,
+				Loai = hh.MaLoaiNavigation.TenLoai
+			}).ToList();
+			return PartialView("TimKiemPartial", data);
+		}
+
+
+	}
+}
